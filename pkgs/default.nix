55af49b8ba9032ec956405d3af084a57865d8e3f
Sefa Eyeoglu <contact@scrumplex.net>
2023-09-01 12:54:25 +0200

    pkgs: filter packages for systems
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,14 @@
 {
-  perSystem = {pkgs, ...}: {
-    packages = import ./all-packages.nix {} pkgs;
+  perSystem = {
+    lib,
+    pkgs,
+    system,
+    ...
+  }: let
+    allPackages = import ./all-packages.nix {} pkgs;
+    packages = lib.filterAttrs (_: v: builtins.elem system (v.meta.platforms or []) && !(v.meta.broken or false)) allPackages;
+  in {
+    inherit packages;
   };
 
   flake.overlays.default = import ./all-packages.nix;
