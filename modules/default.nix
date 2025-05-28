f736b8efa2c1389abf16f939aa4ff680a1bf2d6d
Sefa Eyeoglu <contact@scrumplex.net>
2025-05-28 19:48:32 +0200

    homeModules: remove jellyfin-mpv-shim in favor of upstream module
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- modules/default.nix
+++ modules/default.nix
@@ -3,7 +3,6 @@
     hmModules = {
       catppuccin = import ./hm/catppuccin.nix;
       fish-theme = import ./hm/fish-theme.nix;
-      jellyfin-mpv-shim = import ./hm/jellyfin-mpv-shim.nix;
       pipewire = import ./hm/pipewire.nix;
       waybar-camera-blank = import ./hm/waybar/camera-blank.nix;
       waybar-pa-mute = import ./hm/waybar/pa-mute.nix;
